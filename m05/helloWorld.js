19ec9f23f6e09afb30b9c32c2868c1ae4dcc3c88
Rylee Leavitt <ryleeleavitt@westernwyoming.edu>
2024-10-09 14:59:28 -0600

    Let's Practice JavaScript Variables

---
+++ m05/helloWorld.js
@@ -0,0 +1,8 @@
+/*
+Student Name: Rylee Leavitt
+File Name: helloWorld.js
+Date: 10/06/24
+*/
+
+let hello = "Hello World";
+console.log(hello);
---
+++ m05/practiceVariables.js
@@ -0,0 +1,36 @@
+/*
+Student Name: Rylee Leavitt
+File Name: practiceVariables.js
+Date: 10/09/24
+*/
+
+/*Declare the following variables (use programming best practices here... 
+like let hiDrT = "Hi Dr. T"; ...for the first one):*/
+
+let hiDrT = "Hi Dr. T!";    //this is a string
+let floatNumber = 4.5;      //this is a float
+let zero = 0;               //this is an integer
+let negativeNumber = -10;   //this is a negative integer
+let activation = true;      //this is Boolean Logic
+
+/*For each variable above, print to the terminal window the type of each variable 
+(use console.log(typeof variable); where variable is replaced with your variable name. 
+You should have five let statements followed by five console.log statements. */
+
+console.log (typeof hiDrT);             //String Variable
+console.log (typeof floatNumber);       //Float Variable
+console.log (typeof zero);              //Integer Variable
+console.log (typeof negativeNumber);    //Negative integer Variable
+console.log (typeof activation);        //Boolean Logic Variable
+
+//Next, run the following statements:
+// I also included my predictions for what the console will output for future refernce
+
+console.log("Hi Dr. T!\nLet's get started!");   // \n is a newline character; this will add the text "Let's get started!" in a new line
+console.log(`half of 100 is ${100 / 2}`);       //uses a template literal to embed the result of 100 / 2 in the string = half of 100 is 50
+console.log("con" + "cat" + "e" + "nate");      //adds the values of the string together = concatenate (links [things] together in a chain or series:)
+console.log(8 * null);                          // Multiplies an integer with no data = 0
+console.log("5" - 1);                           // subtracts an integer from a string = 4
+console.log("5" + 1);                           // Adds a string and an integer = 51
+console.log("five" * 2);                        // multiplies a string by an integer = Not-a-Number
+console.log(false == true);                     // == is the equality operator = false; false does not equal true
